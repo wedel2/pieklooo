9aa1ce50ebab3b0527879ce95ad1c7f8dbff1047
Karol Lewandowski <karollewandowski007@gmail.com>
2026-09-06 14:24:54 +0200

    Update coin.gd
    Poprawiona moneta, dodane komentarze

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,25 +1,26 @@
+class_name coin
 extends Area2D
+##Klasa coin
+##
+##monety które można zebrać i kupić za nie ulepszenia gracza
 
-@export var value: int = 1
-@export var save_id: String = ""
-var collected: bool = false
+
+@export var value: int = 1##wartość monet
+@export var save_id: String = ""##id monet służące do sprawdzenia czy monety były już zebrane 
+var collected: bool = false##pole pomocniczne służące do znikania zebranych monet
 
 func _ready() -> void:
 	if SaveManager.is_pickup_collected(save_id):
 		queue_free()
 		return
 
-func _on_body_entered(body: Node2D) -> void:##przy zebraniu monety dodaj ją do monet gracza i usuń monetę
+func _on_body_entered(body: Node2D) -> void:##przy zebraniu monety dodaj ją do monet gracza, usuń monetę i dodaj do zapisu jako zebraną
 	if collected:
 		return
 
 	if not body.is_in_group("player"):
 		return
 
-	collected = true
-
-	if not body.has_method("add_coins"):
-		return
 	collected = true
 
 	SaveManager.mark_pickup_collected(save_id)
